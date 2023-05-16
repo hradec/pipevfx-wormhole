340b2e9eb757c7fa809da3266dc07b5a8892df3a
rhradec <me@hradec.com>
2023-05-16 15:22:47 -0700

    working version - displays the scripts in ./scripts folder as radio buttons. Select one and it will show the source code on the right, with a RUN and STOP button to run the script or stop it. It displays the script output below the buttons.

--- python/scripts.py
+++ python/scripts.py
@@ -1,62 +1,81 @@
 
+import os, glob, time, subprocess
+
 import streamlit as st
-import os, glob
-
+from st_btn_select import st_btn_select
 import redirect as rd
-import time
 
-# use the full area of the browser - need to be called before everything else
-st. set_page_config(layout="wide")
 
 scripts_folder = os.path.abspath(os.path.dirname(__file__)+'/../')
 
+# use the full area of the browser - need to be called before everything else
+st.set_page_config(layout="wide", initial_sidebar_state="expanded", page_title="pipevfx-wormhole")
 
-
-# left_column, right_column = st.columns([1])
-# # You can use a column just like st.sidebar:
-# def button():
-#     # with st.spinner("please wait"):
-#     list = glob.glob(os.environ['HOME']+"/*")
-#     os.system("ls -l %s" % os.environ['HOME']+"/*")
-#     # os.system("ls -l %s" % os.environ['HOME']+"/*")
-#     print(list)
-#     # with rd.stdout, rd.stderr(format='markdown', to=st.sidebar):
-#     with rd.stdout(max_buffer=1024, to=left_column):
-#         for line in list:
-#             print(f"> {line}")
-#             time.sleep(0.01)
-#     # left_column.success("list:\n"+'<BR>'.join(list))
-#
-# left_column.button('Press me!', on_click=button)
-
-# Or even better, call Streamlit functions inside a "with" block:
-# with right_column:
-#     chosen = st.radio(
-#         'Sorting hat',
-#         ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-#     st.write(f"You are in {chosen} house!")
-#
-
-
-
-# Add a selectbox to the sidebar:
-# add_selectbox = st.sidebar.selectbox(
-#     'How would you like to be contacted?',
-#     ('Email', 'Home phone', 'Mobile phone')
-# )
-#
-# # Add a slider to the sidebar:
-# add_slider = st.sidebar.slider(
-#     'Select a range of values',
-#     0.0, 100.0, (25.0, 75.0)
-# )
-def sidebar_runscript( *args ):
-    print(args[0])
-    sidebar()
+# Initialize a session state variable that tracks the sidebar state (either 'expanded' or 'collapsed').
+if 'sidebar_state' not in st.session_state:
+    st.session_state.sidebar_state = 'expanded'
 
 def sidebar():
     with st.sidebar:
         buttons = []
+        dbuttons = {}
         for each in glob.glob( f"{scripts_folder}/scripts/*" ):
-            buttons += [ st.button( os.path.basename(each), on_click=sidebar_runscript, args=[each] ) ]
-sidebar()
+            each = os.path.abspath(each)
+            if os.path.isfile(each):
+                # buttons += [ st.radio( os.path.basename(each), on_change=sidebar_runscript, args=[each] ) ]
+                buttons += [ each ]
+                dbuttons[ os.path.basename(each) ] = each
+
+        selected = 0
+        if hasattr(st.session_state, 'script'):
+            if st.session_state.script in dbuttons.keys():
+                selected = dbuttons.keys().index( st.session_state.script )
+
+        script = st.radio('select the scritp to run:', options=dbuttons.keys(), index = selected)
+        ret = dbuttons[script]
+        st.session_state.script = ret
+        return ret
+
+script = sidebar()
+#cols = st.columns(4,gap="small")
+st.write("## %s" % os.path.basename(script), )
+selection = ''
+selection = st_btn_select( ("SHOW CODE", "RUN", "STOP"), index=0, nav=False )
+st.divider()
+
+print(selection)
+if selection == "SHOW CODE":
+    code = ''.join(open(script, 'r').readlines())
+    st.code(code, language='bash')
+
+elif selection == "RUN":
+        with rd.stdout():
+            proc = subprocess.Popen( [script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print("="*200)
+            while True:
+                status = proc.poll()
+                if status == None:
+                    try:
+                        outs, errs = proc.communicate(timeout=15)
+                    except TimeoutExpired:
+                        proc.kill()
+                        outs, errs = proc.communicate()
+                    print(type(outs))
+                    for l in outs.splitlines():
+                        print( l.decode("utf-8")  )
+                    #print( errs )
+                elif status == 0:
+                    # harvest the answers
+                    print("=" * 200)
+                    print( "finished susscessfully - return code:", status)
+                    break
+                else:
+                    print("=" * 200)
+                    print( "command failed - return code:", status )
+                    break
+                time.sleep(0.1)
+            print("="*200)
+
+elif selection == "STOP":
+
+    pass
