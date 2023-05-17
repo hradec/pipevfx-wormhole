5cd143401baf11181910d54a20c550466857f251
rhradec <me@hradec.com>
2023-05-16 17:12:47 -0700

    added -s option to run a shell in the docker container.

--- webserver.sh
+++ webserver.sh
@@ -10,6 +10,10 @@ if [ "$1" == "-l" ] ; then
 	docker logs -f streamlit
 	exit 0
 fi
+if [ "$1" == "-s" ] ; then
+	docker exec -ti streamlit /bin/bash
+	exit 0
+fi
 if [ "$1" == "-h" ] ; then
 	echo -e "\n$(basename $0)\n\n\t -d = run in daemon mode\n\t -l = display daemon log\n\n"
 	exit 0
