b14529237a1de85933999acf2a04c83e1c59e432
rhradec <me@hradec.com>
2023-05-16 17:12:26 -0700

    fix volumes to map the actual mount instead of the full path. (this allows for pipevfx code to work!)

--- tools/run-docker.sh
+++ tools/run-docker.sh
@@ -14,6 +14,7 @@ for each in $(ls -1 /*/.root) ; do
 done
 for each in $(ls -1 $CD/scripts/*) ; do
 	abs=$(dirname $(readlink -f $each))
+	abs=$(df -h $abs | grep -v Size | awk '{print $(NF)}')
 	if [ "$(echo $volumes | grep $abs)" == "" ] ; then
 		volumes="$volumes -v $abs:$abs "
 	fi
@@ -22,11 +23,14 @@ done
 docker build $CD/docker/ -t hradec/streamlit
 docker rm -f streamlit
 docker run --name streamlit $extra \
+	--shm-size=16g \
 	--net host \
 	-e HOME=$HOME \
 	-e USER=$USER \
 	$volumes \
+	-v /etc/shadow:/etc/shadow \
 	-v /etc/passwd:/etc/passwd \
+	-v /etc/sudoers:/etc/sudoers \
 	-v /home/$USER:$(readlink -f /home/$USER) \
 	-v $CD:$CD \
 	-v $CD:/data \
